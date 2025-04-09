43864df899f0b7668a3e1df9b26a7950fa4eb897
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-04-09 15:41:52 +0800

    Add some details about the creator and modified some parts

--- quiz_creator.py
+++ quiz_creator.py
@@ -6,11 +6,13 @@ init(autoreset=True)
 # Display the banner
 def display_banner():
     banner = pyfiglet.figlet_format("Quiz Creator", font="slant")
-    print(Style.BRIGHT + Fore.BLUE + banner)
+    print(Style.BRIGHT + Fore.LIGHTMAGENTA_EX + banner)
 
 def main():
     display_banner()
-    print(Fore.MAGENTA + "Welcome to the Quiz Creator! Type 'exit' anytime to stop.\n")
+    print(Fore.MAGENTA + "Welcome to the Quiz Creator!")
+    print(Fore.LIGHTMAGENTA_EX + "Created by: Jonalyn Antoc from BSCpE 1-1")
+    print(Fore.MAGENTA + "Type 'exit' anytime to stop.\n")
 
 if __name__ == "__main__":
     main()
