48fd6885a5962c125cbe6f6dd9797b3ef566992b
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-04-09 16:05:28 +0800

    Ask the user to input a question

--- quiz_creator.py
+++ quiz_creator.py
@@ -13,6 +13,14 @@ def main():
     print(Fore.MAGENTA + "Welcome to the Quiz Creator!")
     print(Fore.LIGHTMAGENTA_EX + "Created by: Jonalyn Antoc from BSCpE 1-1")
     print(Fore.MAGENTA + "Type 'exit' anytime to stop.\n")
+    
+    question_count = 0
+    
+    while True:
+        # Get question input from the user
+        question_text = input(Fore.BLUE + "Enter your question: ").strip()
+        if question_text.lower() == 'exit':
+            break
 
 if __name__ == "__main__":
     main()
