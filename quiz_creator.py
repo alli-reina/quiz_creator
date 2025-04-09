b53ed8b0fb60ea181c7441b29aad893eca14400b
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-04-09 15:23:37 +0800

    Add banner using pyfiglet and bright blue text with colorama

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,15 @@
+import pyfiglet
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+# Display the banner
+def display_banner():
+    banner = pyfiglet.figlet_format("Quiz Creator", font="slant")
+    print(Style.BRIGHT + Fore.BLUE + banner)
+
 def main():
-    pass
+    display_banner()
 
 if __name__ == "__main__":
     main()
